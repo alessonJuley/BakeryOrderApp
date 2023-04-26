1326fe53d3e08e363715e23095b4a12c439382cf
alessonJuley <alessonabao@gmail.com>
2023-04-26 20:41:16 +1200

    added frontend login and register with core data entities

---
+++ BakeryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BakeryApp
+//
+//  Created by Alesson Abao on 25/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
